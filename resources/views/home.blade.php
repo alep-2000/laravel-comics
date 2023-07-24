339ad1b11a79c7cb3d104defdca7be583120c135
alep-2000 <alessiopiras056@gmail.com>
2023-07-24 14:50:16 +0200

    Add yield and section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel DC Comics</title>
-    
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-    @include('partials.header')
-    <div class="jumbotron">
-        <div>
-            <strong>CURRENT SERIES</strong>
-        </div>
-    </div>
-    <main class="bg-dark">
-       <div class="container">
+@section('content')
+    <div class="container">
             <div class="row">
                 @foreach($comics as $comic)
                 <div class="col-2 mt-5">
@@ -38,9 +17,5 @@
                     <button class="button"><strong>LOAD MORE</strong></button>
                 </div>
             </div>
-       </div>
-    </main>
-    @include('partials.footer')
-</body>
-
-</html>
+    </div>
+@endsection
