0b70a2201f0ae168d0c8f31801c223af47a6ad2e
alep-2000 <alessiopiras056@gmail.com>
2023-07-24 15:58:13 +0200

    show

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-               
+    <section class="bg-primary py-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                <div class="current">
+                        <img src=" {{ $comic['thumb'] }} " alt="">
+                </div>
+                </div>
             </div>
         </div>
-    </div>
+    </section>
 @endsection
